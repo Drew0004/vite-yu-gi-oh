ccdf4a46ed5265e876ff711ff35128c2c0dca1fe
Andrea Aliquò <146461081+Drew0004@users.noreply.github.com>
2024-01-22 14:20:45 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+
+        };
+    },
+    components: {
+        AppHeader,
+        AppMain,
+        AppFooter
+    },  
+    methods: {
+
+    },
+    mounted(){
+        axios.get()
+        .then();
+    }
+}
+</script>
+
+<template>
+    <h1>
+        Mia App
+    </h1>
+
+    <AppHeader />
+
+    <AppMain />
+
+    <AppFooter />
+</template>
+
+<style lang="scss">
+@use "assets/scss/partials/reset" as *;
+@use "assets/scss/main" as *;
+</style>
