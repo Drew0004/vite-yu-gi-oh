2bf4d97c4cc25c9d7873292360c09ac602814e2c
Drew0004 <drdrewgraphic@gmail.com>
2024-01-22 17:13:52 +0100

    Add loading component

--- src/App.vue
+++ src/App.vue
@@ -2,39 +2,51 @@
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
 import AppFooter from './components/AppFooter.vue';
+import LoadingScreen from './components/LoadingScreen.vue'
 import axios from 'axios';
 import { store } from './store.js';
 
 export default {
     data() {
         return {
-            store
+            store,
+            loading: true
         };
     },
     components: {
         AppHeader,
         AppMain,
-        AppFooter
+        AppFooter,
+        LoadingScreen
     },  
     methods: {
 
     },
     created(){
+        setTimeout(()=>{
+            this.loading = false;
+        }, 2000);
+        
+    },
+    mounted(){
         axios.get(this.store.baseUrl)
         .then((response)=>{
             console.log(response);
             this.store.cards = response.data.data;
             console.log(this.store.cards);
+            
         });
-    }
+    },
 }
 </script>
 
 <template>
 
-    <AppHeader />
+    <LoadingScreen v-if="loading"/>
 
-    <AppMain />
+    <AppHeader v-if="loading == false" />
+
+    <AppMain v-if="loading == false" />
 
 </template>
 
---
+++ src/components/LoadingScreen.vue
@@ -0,0 +1,40 @@
+<script>
+export default {
+    data() {
+        return {
+
+        };
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<template>
+      <div class="loading">
+        <img class="img" src="../../public/img/Spinner-1s-200px.gif" alt="loading..."/>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    @use "../assets/scss/partials/variables.scss" as *;
+    @use "../assets/scss/partials/mixins.scss" as *;
+
+    .loading {
+        position: relative;
+        z-index: 1;
+        background-color: white;
+        height: 100vh;
+        width: 100vw;
+        text-align: center;
+        line-height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .img {
+        position: absolute;
+        margin: auto
+    }
+</style>
