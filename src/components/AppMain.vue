1992aaa0309fbd62d98a74ff95dbbfe60c9fd140
Drew0004 <drdrewgraphic@gmail.com>
2024-01-22 15:29:21 +0100

    Add singleCard prop and stamp

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,23 +1,58 @@
 <script>
+import { store } from '../store.js';
+import SingleCard from './SingleCard.vue';
 export default {
     data() {
         return {
-
+            store
         };
     },
-    methods: {
-
+    components: {
+        SingleCard
     }
 }
 </script>
 
 <template>
+    
     <main>
-        MAIN
+        
+        <div class="container">
+            
+            <div class="py-4">
+                <select class="form-select w-25" aria-label="Default select example">
+                    <option selected>Alien</option>
+                    <option value="1">Option 1</option>
+                    <option value="2">Option 2</option>
+                    <option value="3">Option 3</option>
+                </select>
+            </div>
+
+            <!-- white box -->
+            <div class="bg-white">
+                <div class="container p-4">
+                    <div class="col-12 bg-dark p-3">
+                        <h3 class="text-white fs-5">Found tot cards</h3>
+                    </div>
+
+                    <div>
+                        <SingleCard v-for="(card, i) in store.cards" :card="card"/>
+                    </div>
+                </div>
+
+            </div>
+        
+        </div>
+    
     </main>
+
 </template>
 
 <style lang="scss" scoped>
     @use "../assets/scss/partials/variables.scss" as *;
     @use "../assets/scss/partials/mixins.scss" as *;
+
+    main{
+        background-color: $bg_color;
+    }
 </style>
---
+++ src/components/SingleCard.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+    data() {
+        return {
+
+        };
+    },
+    props: {
+        card: Object,
+    }
+}
+</script>
+
+<template>
+    <div>
+        <img :src="card.card_images[0].image_url" alt="">
+    </div>
+
+    <div>
+        {{ card.name }}
+    </div>
+
+    <div>
+        {{ card.archetype }}
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    @use "../assets/scss/partials/variables.scss" as *;
+    @use "../assets/scss/partials/mixins.scss" as *;
+</style>
