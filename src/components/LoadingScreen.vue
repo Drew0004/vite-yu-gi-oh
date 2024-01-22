5c75a1e55cc37c599641056bdbb81c0e0de51835
Drew0004 <drdrewgraphic@gmail.com>
2024-01-22 17:19:35 +0100

    Fix load component style

--- src/components/LoadingScreen.vue
+++ src/components/LoadingScreen.vue
@@ -13,7 +13,7 @@ export default {
 
 <template>
       <div class="loading">
-        <img class="img" src="../../public/img/Spinner-1s-200px.gif" alt="loading..."/>
+        <img src="../../public/img/Spinner-1s-200px.gif" alt="loading..."/>
     </div>
 </template>
 
@@ -22,19 +22,12 @@ export default {
     @use "../assets/scss/partials/mixins.scss" as *;
 
     .loading {
-        position: relative;
         z-index: 1;
         background-color: white;
         height: 100vh;
         width: 100vw;
-        text-align: center;
-        line-height: 100vh;
         display: flex;
         justify-content: center;
         align-items: center;
-    }
-    .img {
-        position: absolute;
-        margin: auto
     }
 </style>
