281aefd1a9e25289fe4291357af4abac37c8aff4
Drew0004 <drdrewgraphic@gmail.com>
2024-01-22 14:36:40 +0100

    Get api request

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,12 @@ import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
 import AppFooter from './components/AppFooter.vue';
 import axios from 'axios';
+import { store } from './store.js';
 
 export default {
     data() {
         return {
-
+            store
         };
     },
     components: {
@@ -19,8 +20,12 @@ export default {
 
     },
     mounted(){
-        axios.get()
-        .then();
+        axios.get(this.store.baseUrl)
+        .then((response)=>{
+            console.log(response);
+            this.store.cards = response.data.data;
+            console.log(this.store.cards);
+        });
     }
 }
 </script>
